55ba573662c7eb51f9ea6d0d5df2ebcf79119f29
caonhathao <caonhathao2372004@gmail.com>
2025-09-05 00:31:02 +0700

    Update - Change to Shadcn

--- app/[locale]/user/login/page.tsx
+++ app/[locale]/user/login/page.tsx
@@ -5,7 +5,9 @@ import {useTranslations} from "next-intl";
 import Link from "next/link";
 import {Formik} from "formik";
 import * as Yup from "yup";
-import {TextField} from "@mui/material";
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
 
 export default function UserLoginPage() {
     const t = useTranslations("login");
@@ -44,14 +46,16 @@ export default function UserLoginPage() {
                           errors,
                       }) => {
                         return (
-                            <form className="w-[70%] h-fit m-2 p-2 shadow-lg rounded-xl flex flex-col justify-center items-center gap-3"
-                            onSubmit={handleSubmit}>
-                                    <TextField
-                                        variant={'outlined'}
+                            <form
+                                className="w-[70%] h-fit m-2 p-2 shadow-lg rounded-xl flex flex-col justify-center items-center gap-3"
+                                onSubmit={handleSubmit}>
+                                <div className={'w-full'}>
+                                    <Label htmlFor="email">{t("email")}</Label>
+                                    <Input
                                         name={'email'}
-                                        className="w-full p-0  placeholder-red-500 placeholder:italic"
+                                        className="w-full p-2"
                                         type="text"
-                                        label={t("email")}
+                                        placeholder={t("email")}
                                         required
                                         onChange={(e) => {
                                             handleChange(e);
@@ -66,13 +70,14 @@ export default function UserLoginPage() {
                                             }
                                         }}
                                     />
-
-                                    <TextField
-                                        variant={'outlined'}
+                                </div>
+                                <div className={'w-full'}>
+                                    <Label htmlFor="email">{t("password")}</Label>
+                                    <Input
                                         name={'password'}
-                                        className="w-full p-0 outline-none"
+                                        className="w-full p-2"
                                         type="password"
-                                        label={t("password")}
+                                        placeholder={t("password")}
                                         required
                                         onChange={(e) => {
                                             handleChange(e);
@@ -87,42 +92,52 @@ export default function UserLoginPage() {
                                             }
                                         }}
                                     />
+                                </div>
 
                                 <div className="w-full h-fit p-2 grid grid-cols-2 grid-rows-1 gap-2">
-                                    <button
+                                    <Button
+                                        variant={"ghost"}
                                         type="button"
                                         className="border-2 border-[var(--primary)] rounded-xl p-2"
                                     >
                                         <Link href={"/user/register"}>{t("register")}</Link>
-                                    </button>
-                                    <button
+                                    </Button>
+                                    <Button
                                         type="submit"
                                         className="bg-[var(--primary)] rounded-xl p-2 text-[var(--primary-foreground)]"
                                     >
                                         {t("login")}
-                                    </button>
+                                    </Button>
                                 </div>
                             </form>
                         );
                     }}
                 </Formik>
 
-                <p className={'text-[var(--primary)]'}>Hoặc đăng nhập bằng</p>
-                <div className="w-[70%] h-fit m-2 p-2 shadow-lg rounded-lg grid grid-cols-2 grid-rows-1 gap-2">
-                    <button
-                        className="w-full h-fit p-2 my-2 border-2 border-[var(--primary)] rounded-lg flex flex-row justify-between items-center gap-2">
-                        <div className="w-[20%]">
+                <div
+                    className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                        <span className="bg-background text-muted-foreground relative z-10 px-2">
+                            {t("others_way")}
+                        </span>
+                </div>
+                <div
+                    className="w-[70%] h-fit m-2 p-2 shadow-lg rounded-lg grid md:grid-cols-1 md:grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-2">
+                    <Button
+                        variant={'outline'}
+                        className="w-full h-fit p-2 my-2 border-2 border-[var(--primary)] rounded-lg flex flex-row justify-center items-center gap-2">
+                        <div className="w-fit">
                             <FaGooglePlusG size={30} color={"var(--primary)"}/>
                         </div>
-                        <p className="w-[80%] text-left">{t("login_google")}</p>
-                    </button>
-                    <button
-                        className="w-full h-fit p-2 my-2 border-2 border-[var(--primary)] rounded-lg flex flex-row justify-between items-center gap-2">
-                        <div className="w-[20%]">
+                        <p className="w-fit text-left">{t("login_google")}</p>
+                    </Button>
+                    <Button
+                        variant={'outline'}
+                        className="w-full h-fit p-2 my-2 border-2 border-[var(--primary)] rounded-lg flex flex-row justify-center items-center gap-2">
+                        <div className="w-fit">
                             <FaApple size={30} color={"var(--primary)"}/>
                         </div>
-                        <p className="w-[80%] text-left">{t("login_apple")}</p>
-                    </button>
+                        <p className="w-fit text-left">{t("login_apple")}</p>
+                    </Button>
                 </div>
             </div>
         </div>
---
+++ app/components/custom-ui/select-language.tsx
@@ -0,0 +1,65 @@
+"use client"
+import * as React from "react"
+import {Button} from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup, DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {usePathname, useRouter} from "next/navigation";
+import {useLocale} from "next-intl";
+
+export const SelectLanguage = ({t}: { t: (key: string) => string }) => {
+    const locale = useLocale();
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const changeLang = (event: string) => {
+        const lang = event
+        console.log(lang)
+        const withoutLocale = pathname.replace(/^\/(vi|en|jp)(\/|$)/, "/");
+        router.push(`/${lang}${withoutLocale}`);
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">{t("language")}</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuSeparator/>
+                <DropdownMenuRadioGroup value={locale} onValueChange={(val) => changeLang(val)}>
+                    <DropdownMenuRadioItem
+                        value={'vi'}
+                    >
+                        <div className="flex flex-row justify-center items-center gap-2">
+                            <img width="48" height="48" src="https://img.icons8.com/color/48/vietnam.png"
+                                 alt="vietnam"/>
+                            <p>Tiếng Việt</p>
+                        </div>
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem
+                        value={'en'}
+                    >
+                        <div className="flex flex-row justify-center items-center gap-2">
+                            <img width="48" height="48" src="https://img.icons8.com/color/48/great-britain.png"
+                                 alt="great-britain"/>
+                            <p>English</p>
+                        </div>
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem
+                        value={'jp'}
+                    >
+                        <div className="flex flex-row justify-center items-center gap-2">
+                            <img width="48" height="48" src="https://img.icons8.com/?size=100&id=22435&format=png&color=000000"
+                                 alt="japan"/>
+                            <p>日本語</p>
+                        </div>
+                    </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
